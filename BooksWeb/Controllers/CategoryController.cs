0999b22bf889930411be313c2bf7b8f7a11fbd3a
kreskeskes <travojuika@gmail.com>
2023-04-21 04:17:43 +0300

    Created project, added configurations for database, showing and creating new book categories
    -Create the project
    -Create Category Model
    -Install NuGet Packages
    -Add ConnectionStrings to appsettings.json
    -Add ApplicationDbContext
    -Add services for database to Program.cs
    -Create database
    -Create Category table using DbSet
    -Add Category Controller
    -Add Category to navbar
    -Seed Data for Category using OnModelCreating and ModelBuilder
    -Retrieve Category items from Database in CategoryController
    -Add UI for Category table, populate the fields with data retrieved from Database using @model
    -Add BootsWatch Theme
    -Add action method Create in CategoryController
    -Add Create Category UI

---
+++ BooksWeb/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using BooksWeb.Data;
+using BooksWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWeb.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			List<Category> objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			_db.Categories.Add(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}

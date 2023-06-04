291a0f0e86458f0c77726ff0d69d8edf90c2ee29
Nelika13 <nellya.sikil@mail.ru>
2023-06-04 19:57:19 +0300

    Add OrderController and UI for Index and Details Action Methods, retrieve and display order data from DB
    -Add OrderController to Admin Area and fill it with API call
    -Add OrderVM and fill it with OrderHeader and Ienumerable or OrderDetail
    -Add an Index View for OrderController
    -Add order.js file for DataTables table and fill it
    -Add Manage Order to navbar to _Layout
    -Add Order Status filter on Order Index by changing UI and adding functionalities in OrderController and order.js
    -Add Details action Method to retrieve data on order in OrderController
    -Add and design Details UI and retrieves all the data from OrderHeader

---
+++ Books.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿using Books.Models.Models;
+
+namespace Books.Models.ViewModels
+{
+	public class OrderVM
+	{
+		public OrderHeader OrderHeader { get; set; }
+		public IEnumerable<OrderDetail> OrderDetail { get; set; }
+	}
+
+}

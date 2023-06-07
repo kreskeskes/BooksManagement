219a17a31ee1b545f1407e8c9fecdb57e46e8a43
savaggge <alexandr.vagheliuc@isa.utm.md>
2023-06-07 16:26:52 +0300

    Add Feature to Manage Users, Lock, Unlock and Change the Roles.
    -Add UserController, view and user.js to fill it with user data
    -Add functionality to display role on the list
    -Add LockUnlock Action Method to UserController
    -Implement buttons and add a switch logic for them in appUser.js
    -Add RoleManagement action method to UserController and implement it
    -Add UI for RoleManagement
    -Add Post action method for RoleManagement to update the current user

--- Books.Models/Models/ApplicationUser.cs
+++ Books.Models/Models/ApplicationUser.cs
@@ -6,19 +6,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Books.Models.Models
 {
-    public class ApplicationUser : IdentityUser
-    {
-        [Required]
-        public string Name { get; set; }
-        public string? StreetAddress { get; set; }
-        public string? City { get; set; }
-        public string? PostalCode { get; set; }
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		public string Name { get; set; }
+		public string? StreetAddress { get; set; }
+		public string? City { get; set; }
+		public string? PostalCode { get; set; }
 
-        public int? CompanyId { get; set; }
+		public int? CompanyId { get; set; }
 
-        [ValidateNever]
-        [DisplayName("Company ID")]
-        [ForeignKey("CompanyId")]
-        public Company Company { get; set; }
-    }
+		[ValidateNever]
+		[DisplayName("Company ID")]
+		[ForeignKey("CompanyId")]
+		public Company? Company { get; set; }
+		[NotMapped]
+		public string Role { get; set; }
+	}
 }
---
+++ Books.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,13 @@
+﻿using Books.Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Books.Models.ViewModels
+{
+	public class RoleManagementVM
+	{
+		public ApplicationUser ApplicationUser { get; set; }
+		public IEnumerable<SelectListItem> CompanyList { get; set; }
+		public IEnumerable<SelectListItem> RoleList { get; set; }
+
+	}
+}
